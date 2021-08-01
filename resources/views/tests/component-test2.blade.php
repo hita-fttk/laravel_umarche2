bedbcf41c0b2edcbab9bfa645a97e0d57037225b
hita-fttk <fttkmcc77@gmail.com>
2021-08-01 11:38:08 +0900

    classBaseを登録してみました

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,6 +1,9 @@
 <x-tests.app>
-    
+
 <x-slot name="header">ヘッダー２</x-slot> 
 コンポーネントテスト２
+<x-test-class-base classBaseMessage="メッセージです" />
+<div class="mb-4"></div>
+<x-test-class-base classBaseMessage="メッセージを変更をしてみました" defaultMessage="初期値を変更してみました" />
 </x-tests.app>
 
