76486d59d4ac232946da5158c7434297a60a166d
hita-fttk <fttkmcc77@gmail.com>
2021-08-27 20:31:04 +0900

    edit update

--- app/Http/Controllers/Owener/ImageController.php
+++ app/Http/Controllers/Owener/ImageController.php
@@ -85,10 +85,7 @@ class ImageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
+
 
     /**
      * Show the form for editing the specified resource.
@@ -98,7 +95,8 @@ class ImageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $image = Image::findOrFail($id);
+        return view('owener.images.edit',compact('image'));
     }
 
     /**
@@ -110,7 +108,17 @@ class ImageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => 'string|max:50'
+        ]);
+
+        $image = Image::FindOrFail($id);
+        $image->title = $request->title;
+        $image->save();
+
+        return redirect()->route('owener.images.index')
+        ->with(['message'=> '画像情報を更新しました。',
+                'status' => 'info']);
     }
 
     /**
