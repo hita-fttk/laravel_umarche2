44ecb7c248b88de862581ea11dd9345f3c0b537f
hita-fttk <fttkmcc77@gmail.com>
2021-09-05 11:23:33 +0900

    swiper

---
+++ resources/js/swiper.js
@@ -0,0 +1,32 @@
+//   // import Swiper JS
+//   import Swiper from 'swiper';
+  // import Swiper styles
+  import 'swiper/css';
+
+    // core version + navigation, pagination modules:
+    import Swiper, { Navigation, Pagination } from 'swiper';
+  
+    // configure Swiper to use modules
+    Swiper.use([Navigation, Pagination]);
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        // direction: 'vertical',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
