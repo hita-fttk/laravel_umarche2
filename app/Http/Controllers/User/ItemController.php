817d5e57acb390847fb9fa226a9903c2dabe00d5
hita-fttk <fttkmcc77@gmail.com>
2021-09-08 19:22:00 +0900

    pagination default

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -31,7 +31,7 @@ class ItemController extends Controller
     {
         $products = Product::availableItems()
         ->sortOrder($request->sort)
-        ->paginate($request->pagination);
+        ->paginate($request->pagination ?? '20');
         // dd($stocks,$products);
         // $products = Product::all();
         return view('user.index', compact('products'));
